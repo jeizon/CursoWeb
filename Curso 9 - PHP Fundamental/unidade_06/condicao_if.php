772aaa9834d163e8954c99f4ff4334e2edcd9ef8
jeizon <jeydroids@gmail.com>
2018-12-07 07:14:41 -0200

    9/6 - Estrutura condicional IF
    Seção 70, aula 306
    
    Curso 9 - Curso PHP Fundamental
    Unidade 06 - Estruturas de Controle

---
+++ Curso 9 - PHP Fundamental/unidade_06/condicao_if.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <?php
+        $a = 5;
+        $b = 3;
+        if ( $a > $b ) {
+            echo "A é maior que B" . "<br>";
+        }
+
+    $fumante = false;
+    if ( !$fumante  ) {
+        echo "Sim, podera entrar" . "<br>";
+    }
+    ?>
+    </body>
+</html>
