4b0dec1cbccd0cd22a2a5700a3915c705001958f
jeizon <jeydroids@gmail.com>
2018-12-07 02:26:51 -0200

    7/04 -  Estatísticas do vídeo
    Seção 60, aula 267
    
    Curso 7 - Curso Youtube - API V3
    Unidade 04 - Estatísticas do vídeo

--- Curso 7 - Youtube API/unidade03/js/meuscript.js
+++ Curso 7 - Youtube API/unidade03/js/meuscript.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $.get("", {
-            part: '',
-            id: '',
-            key: 'AIzaSyB49WfTkgfK2menTbmVCkLG0f9cYWQ9XKU'},
+    $.get("https://www.googleapis.com/youtube/v3/videos", {
+            part: 'statistics',
+            id: 'pV7pMS0xirU',
+            key: 'AIzaSyCke91lY6SnYELGZ_6BPxvlEThhVo2lDk8'},
             function(data) {
                 console.log(data);
             }
