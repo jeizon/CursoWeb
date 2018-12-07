6fedae77c80f33ed1c2a2b76ef6391bb7d08b7ea
jeizon <jeydroids@gmail.com>
2018-12-07 05:37:55 -0200

    9/5 - Números inteiros
    Seção 69, aula 295
    
    Curso 9 - Curso PHP Fundamental
    Unidade 05 - Criando variáveis

--- Curso 9 - PHP Fundamental/unidade_05/numeros1.php
+++ Curso 9 - PHP Fundamental/unidade_05/numeros1.php
@@ -1,29 +1,34 @@
-<?php 
-    $salario = 800;
-    $meses   = 3;
+<?php
+$salario = 800;
+$meses   = 3;
 ?>
 
 <!doctype html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Curso PHP FUNDAMENTAL</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP FUNDAMENTAL</title>
+</head>
 
-    <body>
-        <?php 
-            // Multiplicacao e Divisao
+<body>
+<?php
+// Multiplicacao e Divisao
+echo "Trimestre: " . $salario * $meses . "</br>";
+echo "Quinzena: " . $salario / 2 . "</br>";
+// Exponencial
+echo "Exponencial: " . pow (6,2). "</br>";
+echo "Exponencial: " . pow (5,3). "</br>";
 
-            // Exponencial
+// Raiz Quadrada
+echo "Raiz Quadrada: " . sqrt (36). "</br>";
 
-            // Raiz Quadrada
+// Randômico Generica
+echo "Randomico: ". rand() . "</br>";
 
-            // Randômico Generica
-
-            // Randômico entre um intervalo
-
-            // Valor absoluto
-            
-        ?>
-    </body>
+// Randômico entre um intervalo
+echo "Randomico: ". rand(1,10) . "</br>";
+// Valor absoluto
+echo "Absoluto: " . abs(-10)
+?>
+</body>
 </html>
