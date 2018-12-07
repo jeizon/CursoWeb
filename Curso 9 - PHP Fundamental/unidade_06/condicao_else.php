262df708b99a02d66ea34b792a52697de02fd3e2
jeizon <jeydroids@gmail.com>
2018-12-07 07:23:17 -0200

    9/6 - Estrutura condicional ELSE e ELSE IF
    Seção 70, aula 307
    
    Curso 9 - Curso PHP Fundamental
    Unidade 06 - Estruturas de Controle

---
+++ Curso 9 - PHP Fundamental/unidade_06/condicao_else.php
@@ -0,0 +1,23 @@
+<?php
+    $a = 5;
+    $b = 5;
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <?php
+        if ($a > $b) {
+            echo "A é maior de B" . "<br>";
+        } else if ($a < $b){
+            echo "B é maior que A" . "<br>";
+        } else {
+            echo "A é igual a B" . "<br>";
+        }
+    ?>
+    </body>
+</html>
