bf4a6c9618ae113fccfb47f7add95fdbc9f64027
jeizon <jeydroids@gmail.com>
2018-12-07 06:38:54 -0200

    9/5 - NULL
    Seção 69, aula 302
    
    Curso 9 - Curso PHP Fundamental
    Unidade 05 - Criando variáveis

---
+++ Curso 9 - PHP Fundamental/unidade_05/null.php
@@ -0,0 +1,16 @@
+<?php
+    $nome = null;
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <?php
+        echo "A variavel é null? " . is_null($nome) . "<br>";
+    ?>
+    </body>
+</html>
