b9b506a58c8ba1f6650b9685606ed6d52ac053b5
jeizon <jeydroids@gmail.com>
2018-12-07 06:48:12 -0200

    9/5 - Empty
    Seção 69, aula 303
    
    Curso 9 - Curso PHP Fundamental
    Unidade 05 - Criando variáveis

---
+++ Curso 9 - PHP Fundamental/unidade_05/empty.php
@@ -0,0 +1,18 @@
+<?php
+$nome = null;
+$endereco = "";
+?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP FUNDAMENTAL</title>
+</head>
+
+<body>
+<?php
+echo "A variavel é vazio? " . empty($nome) . "<br>";
+echo "A variavel é vazio? " . empty($endereco) . "<br>";
+?>
+</body>
+</html>
