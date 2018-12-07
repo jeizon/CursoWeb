21a5c2b636c836bdbd99783b78190cae7dc07c7e
jeizon <jeydroids@gmail.com>
2018-12-07 09:28:49 -0200

    9/8 - Função com retorno
    Seção 72, aula 319
    
    Curso 9 - Curso PHP Fundamental
    Unidade 08 - Funções

---
+++ Curso 9 - PHP Fundamental/unidade_08/funcao1.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <?php
+        function retornarDiaria() {
+            $salario = 1110;
+            return number_format($salario/30,2);
+        }
+
+        echo retornarDiaria();
+    ?>
+    </body>
+</html>
