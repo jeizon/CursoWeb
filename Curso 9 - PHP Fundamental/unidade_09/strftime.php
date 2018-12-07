8f728bb0062fff5864516312d766138c46f78714
jeizon <jeydroids@gmail.com>
2018-12-07 10:41:45 -0200

    9/9 - Como mostrar data em português
    Seção 73, aula 325
    
    Curso 9 - Curso PHP Fundamental
    Unidade 09 - Como formatar datas

---
+++ Curso 9 - PHP Fundamental/unidade_09/strftime.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+    <?php
+        // Determinar timezone
+        date_default_timezone_set('America/Sao_Paulo');
+        setlocale(LC_TIME, "pt_BR");
+
+
+        // Criar elementos
+        $ano = strftime('%Y');
+        $mes = strftime('%B');
+        $dia = strftime('%d');
+        $dia_semana = strftime('%A');
+
+        $hora  = strftime('%H');
+        $minuto  = strftime('%M');
+        $segundo  = strftime('%S');
+
+        echo $dia_semana . ", " . $dia . " de " . $mes . " de " . $ano . " - " . $hora . ":" . $minuto . ":" . $segundo;
+    ?>
+    </body>
+</html>
