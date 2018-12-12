70091495d8ee61a6af9e105126ba6c4fb4260982
jeizon <jeydroids@gmail.com>
2018-12-12 03:09:02 -0200

    11/3 - Criar uma consulta a uma tabela
    Seção 88, aula 366
    
    Curso 11 - Curso PHP com MySQL
    Unidade 03 - Como conectar com MySQL

--- Curso 11 - Curso PHP com MySQL/unidade_03/exercicio01.php
+++ Curso 11 - Curso PHP com MySQL/unidade_03/exercicio01.php
@@ -12,17 +12,25 @@
 		die("Conexao falhou: " . mysqli_connect_errno());
 	}
 ?>
-
-<!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Curso PHP FUNDAMENTAL</title>
-    </head>
-
-    <body>
-    </body>
-</html>
+<?php
+	// Passo 3 - Abrir consulta ao banco de dados
+	$consulta_categorias = "SELECT * FROM categorias WHERE categoriaID > 2";
+	$categorias = mysqli_query($conecta, $consulta_categorias);
+	
+	if ( !$categorias ) {
+		die("Falha na consulta ao banco." );
+	}
+?>
+	<!doctype html>
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Curso PHP FUNDAMENTAL</title>
+	</head>
+	
+	<body>
+	</body>
+	</html>
 
 <?php
 	mysqli_close($conecta);
