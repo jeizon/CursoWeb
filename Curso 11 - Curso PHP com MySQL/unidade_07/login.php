0f72d6c16825723c89745f8f0d5ef81b193a689d
jeizon <jeydroids@gmail.com>
2018-12-12 07:02:33 -0200

    12/7 - Criar uma tela de login
    Seção 93, aula 387
    
    Curso 12 - PHP Integração com MySQL - Avançado
    Unidade 07 - Criar um sistema de Login

---
+++ Curso 11 - Curso PHP com MySQL/unidade_07/login.php
@@ -0,0 +1,45 @@
+<?php require_once("../../db/conexao.php"); ?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+        
+        <!-- estilo -->
+        <link href="_css/estilo.css" rel="stylesheet">
+        <link href="_css/login.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+            <div id="header_central">
+                <img src="assets/logo_andes.gif">
+                <img src="assets/text_bnwcoffee.gif">
+            </div>
+        </header>
+        
+        <main>
+			<div id="janela_login">
+				<form action="login.php" method="post">
+					<h2>Tela de Login</h2>
+					<input type="text" name="usuario" placeholder="Usuario">
+					<input type="password" name="senha" placeholder="Senha">
+					<input type="submit" value="Login">
+				</form>
+			</div>
+            
+        </main>
+
+        <footer>
+            <div id="footer_central">
+                <p>ANDES &eacute; uma empresa fict&iacute;cia, usada para o curso PHP Integra&ccedil;&atilde;o com MySQL.</p>
+            </div>
+        </footer>
+    </body>
+</html>
+
+<?php
+    // Fechar conexao
+    mysqli_close($conecta);
+?>
