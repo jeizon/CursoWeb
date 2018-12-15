906f40970089936ad64fdb091dc0d1a3a16c4497
jeizon <jeydroids@gmail.com>
2018-12-15 09:44:02 -0200

    12/11 - Explicando as mensagens de erros
    Seção 97, aula 414
    
    Curso 12 - PHP Integração com MySQL - Avançado
    Unidade 11 - Realizar upload de arquivo

--- Curso 11 - Curso PHP com MySQL/unidade_11/upload.php
+++ Curso 11 - Curso PHP com MySQL/unidade_11/upload.php
@@ -1,11 +1,24 @@
 <?php require_once("../../db/conexao.php"); ?>
 <?php
+	// Erros de upload
+	$array_erro = array(
+		UPLOAD_ERR_OK => "Sem erro.",
+		UPLOAD_ERR_INI_SIZE => "O arquivo enviado excede o limite definido na diretiva upload_max_filesize do php.ini.",
+		UPLOAD_ERR_FORM_SIZE => "O arquivo excede o limite definido em MAX_FILE_SIZE no formulário HTML",
+		UPLOAD_ERR_PARTIAL => "O upload do arquivo foi feito parcialmente.",
+		UPLOAD_ERR_NO_FILE => "Nenhum arquivo foi enviado.",
+		UPLOAD_ERR_NO_TMP_DIR => "Pasta temporária ausente.",
+		UPLOAD_ERR_CANT_WRITE => "Falha em escrever o arquivo em disco.",
+		UPLOAD_ERR_EXTENSION => "Uma extensão do PHP interrompeu o upload do arquivo."
+	);
 	if( isset($_POST["enviar"]) ) {
+		$num_erro = $_FILES['upload_file']['error'];
+		$mensagem = $array_erro[$num_erro];
 		echo "<pre>";
 		print_r($_FILES['upload_file']);
 		echo "</pre>";
-		
-		echo $_FILES['upload_file']['name'];
+		echo "</hr>";
+		echo $mensagem;
 	}
 ?>
 <!doctype html>
@@ -29,6 +42,7 @@
         
         <main>  
             <form action="upload.php" method="post" enctype="multipart/form-data">
+				<input type="hidden" name="MAX_FILE_SIZE" value="600000" >
 				<input type="file" name="upload_file">
 				<input type="submit" name="enviar" value="publicar">
 			</form>
