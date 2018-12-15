ebf17c5d59491ba660d3887228201e5394170806
jeizon <jeydroids@gmail.com>
2018-12-15 05:46:27 -0200

    12/8 - Criar um formulário
    Seção 94, aula 395
    
    Curso 12 - PHP Integração com MySQL - Avançado
    Unidade 08 - Inserindo dados em um B.D.

--- Curso 11 - Curso PHP com MySQL/unidade_08/inserir_transportadoras.php
+++ Curso 11 - Curso PHP com MySQL/unidade_08/inserir_transportadoras.php
@@ -1,35 +1,5 @@
-<?php require_once("../../conexao/conexao.php"); ?>
-<?php
-    // insercao no banco
-    if(isset($_POST["nometransportadora"])) {
-        $nome       = utf8_decode($_POST["nometransportadora"]);
-        $endereco   = utf8_decode($_POST["endereco"]);
-        $cidade     = utf8_decode($_POST["cidade"]);
-        $estado     = $_POST["estados"];
-        $cep        = $_POST["cep"];
-        $cnpj       = $_POST["cnpj"];
-        $telefone   = $_POST["telefone"];
-        
-        $inserir    = "INSERT INTO transportadoras ";
-        $inserir    .= "(nometransportadora,endereco,cidade,estadoID,cep,cnpj,telefone) ";
-        $inserir    .= "VALUES ";
-        $inserir    .= "('$nome','$endereco','$cidade', $estado, '$cep','$cnpj','$telefone')";
-        
-        $operacao_inserir = mysqli_query($conecta,$inserir);
-        if(!$operacao_inserir) {
-            die("Erro no banco");
-        }  
-    }
+<?php require_once("../../db/conexao.php"); ?>
 
-    // selecao de estados
-    $select = "SELECT estadoID, nome ";
-    $select .= "FROM estados ";
-    $lista_estados = mysqli_query($conecta, $select);
-    if(!$lista_estados) {
-        die("Erro no banco");
-    }
-
-?>
 <!doctype html>
 <html>
     <head>
@@ -44,31 +14,24 @@
     <body>
         <?php include_once("_incluir/topo.php"); ?>
         
-        <main> 
+        <main>  
             <div id="janela_formulario">
-            
-                <form action="inserir_transportadoras.php" method="post">
-                    <input type="text" name="nometransportadora" placeholder="Nome da Transportadora">
-                    <input type="text" name="endereco" placeholder="Endereço">
-                    <input type="text" name="telefone" placeholder="Telefone">
-                    <input type="text" name="cidade" placeholder="Cidade">
-                    <select name="estados">
-                        <?php
-                            while($linha = mysqli_fetch_assoc($lista_estados)) {
-                        ?>
-                            <option value="<?php echo $linha["estadoID"];  ?>">
-                                <?php echo utf8_encode($linha["nome"]);  ?>
-                            </option>
-                        <?php
-                            }
-                        ?>                        
-                    </select>
-                    <input type="text" name="cep" placeholder="CEP">
-                    <input type="text" name="cnpj" placeholder="CNPJ">
-                    <input type="submit" value="inserir">
-                </form>
-            </div>
-            
+				<form action="inserir_transportadoras.php" method="post">
+					<input type="text" name="nometransportadora" placeholder="Nome da transportadora : ">
+					<input type="text" name="endereco" placeholder="Digite o endereço : ">
+					<input type="text" name="telefone" placeholder="Telefone : ">
+					<input type="text" name="cidade" placeholder="Cidade : ">
+					<select name="estados">
+						<option value="1">Acre</option>
+						<option value="2">Amapa</option>
+						<option value="3">Amazonia</option>
+					</select>
+					<input type="text" name="cep" placeholder="CEP : ">
+					<input type="text" name="cnpj" placeholder="CNPJ : ">
+					<input type="submit" value="inserir" >
+				</form>
+				
+			</div>
         </main>
 
         <?php include_once("_incluir/rodape.php"); ?>  
