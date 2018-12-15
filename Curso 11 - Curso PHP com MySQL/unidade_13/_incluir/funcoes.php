bb106979303644f7ad42193fe9962637323b08ea
jeizon <jeydroids@gmail.com>
2018-12-15 11:16:37 -0200

    12/13 - Recebendo as variáveis
    Seção 99, aula 423
    
    Curso 12 - PHP Integração com MySQL - Avançado
    Unidade 13 (BÔNUS ) - Contato do site com Mail

--- Curso 11 - Curso PHP com MySQL/unidade_13/_incluir/funcoes.php
+++ Curso 11 - Curso PHP com MySQL/unidade_13/_incluir/funcoes.php
@@ -1,6 +1,20 @@
 <?php
 	function enviarMensagem($dados) {
-		return $dados;
+		// Dados do formulario
+		$nome_usuario		= $dados['nome'];
+		$email_usuario		= $dados['email'];
+		$mensagem_usuario	= $dados['mensagem'];
+		
+		// Criar variaveis de envio
+		$destino			= "teste@localhost.com";
+		$remetente			= "jeizon@jeydroids.desenv";
+		$assunto			= "Mensagem do site";
+		
+		//Montar o corpo da mensagem
+		$mensagem			= "O usuário " . $nome_usuario . "enviou uma mensagem " . "<br>";
+		$mensagem			.= "email do usuário: " . $email_usuario . "<br>";
+		$mensagem			.= "Mensagem:" . "<br>";
+		$mensagem			.= $mensagem_usuario
 	}
 ?>
 
