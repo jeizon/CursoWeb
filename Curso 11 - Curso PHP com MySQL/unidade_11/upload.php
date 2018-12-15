0d9cae27d0a919cfda02a6bd005b2f7b29a25104
jeizon <jeydroids@gmail.com>
2018-12-15 10:05:59 -0200

    12/11 - Otimizar código do upload
    Seção 97, aula 416
    
    Curso 12 - PHP Integração com MySQL - Avançado
    Unidade 11 - Realizar upload de arquivo

--- Curso 11 - Curso PHP com MySQL/unidade_11/upload.php
+++ Curso 11 - Curso PHP com MySQL/unidade_11/upload.php
@@ -1,27 +1,9 @@
 <?php require_once("../../db/conexao.php"); ?>
+<?php require_once("./_incluir/funcoes.php"); ?>
 <?php
-	// Erros de upload
-	$array_erro = array(
-		UPLOAD_ERR_OK => "Sem erro.",
-		UPLOAD_ERR_INI_SIZE => "O arquivo enviado excede o limite definido na diretiva upload_max_filesize do php.ini.",
-		UPLOAD_ERR_FORM_SIZE => "O arquivo excede o limite definido em MAX_FILE_SIZE no formulário HTML",
-		UPLOAD_ERR_PARTIAL => "O upload do arquivo foi feito parcialmente.",
-		UPLOAD_ERR_NO_FILE => "Nenhum arquivo foi enviado.",
-		UPLOAD_ERR_NO_TMP_DIR => "Pasta temporária ausente.",
-		UPLOAD_ERR_CANT_WRITE => "Falha em escrever o arquivo em disco.",
-		UPLOAD_ERR_EXTENSION => "Uma extensão do PHP interrompeu o upload do arquivo."
-	);
+	
 	if( isset($_POST["enviar"]) ) {
-		$arquivo_temp = $_FILES['upload_file']['tmp_name'];
-		$arquivo = basename($_FILES['upload_file']['name']);
-		$diretorio = "uploads";
-		
-		if (move_uploaded_file($arquivo_temp,$diretorio . "/".$arquivo)) {
-			$mensagem = "Arquivo publicado";
-		} else {
-			$num_erro = $_FILES['upload_file']['error'];
-			$mensagem = $array_erro[$num_erro];
-		}
+		$mensagem = publicarArquivo($_FILES['upload_file']);
 	}
 		
 ?>
